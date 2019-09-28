f308ecf14dec5fff8dc8f94bc1c37dc807364695
Jon Sowman <jon@jonsowman.com>
2019-09-28 10:56:18 +0100

    Display wifi connectivity and carbon intensity

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import json
 import energy
 import ssd1306
 import time
-import ntptime
-from machine import Pin, I2C, RTC
+from machine import Pin, I2C
 
 def connect_wifi():
     # Load wifi credentials from file
@@ -26,9 +25,7 @@ def connect_wifi():
     while( not wlan.isconnected() ):
         time.sleep(1)
     print("Connected to {}".format(wifi_ssid))
-
-    # Set NTP
-    ntptime.settime()
+    return wlan
 
 def configure_oled():
     # Configure oled
@@ -36,25 +33,43 @@ def configure_oled():
     oled = ssd1306.SSD1306_I2C(128, 64, i2c)
     return oled
 
-if __name__ == '__main__':
+def update_display(oled, wlan, since_last, mix, ci):
+    oled.fill(0)
+    # Write time
+    oled.text("Last  : {:.0f} secs".format(since_last), 0, 0)
+    if wlan.isconnected():
+        oled.text("Wifi  : OK", 0, 9)
+    else:
+        oled.text("Wifi  : disconn", 0, 9)
+    # Write energy fractions
+    oled.text("Wind  : {:.1f}%".format(mix.percent("wind")), 0, 17)
+    oled.text("Solar : {:.1f}%".format(mix.percent("solar")), 0, 26)
+    oled.text("Bio   : {:.1f}%".format(mix.percent("biomass")), 0, 35)
+    oled.text("Import: {:.1f}%".format(mix.percent("imports")), 0, 44)
+    oled.hline(0, 54, 128, 1)
+    oled.text("Intens: {:.0f}g/kWh".format(ci.current), 0, 56)
+    oled.show()
+
+def run():
     # Run init
-    connect_wifi()
+    wlan = connect_wifi()
     oled = configure_oled()
-    rtc = RTC()
+    first_run = True
+
+    # Track the last update time
+    last_update = time.ticks_ms()
 
     # Run forever
     while True:
-        mix = energy.mix()
-        oled.fill(0)
-        # Write time
-        time_now = rtc.datetime()
-        oled.text("Last: {:02d}:{:02d}:{:02d}".format(time_now[4],
-            time_now[5], time_now[6]), 0, 0)
-        # Write energy fractions
-        oled.text("Wind: {}%".format(mix.percent("wind")), 0, 20)
-        oled.text("Solar: {}%".format(mix.percent("solar")), 0, 30)
-        oled.text("Hydro: {}%".format(mix.percent("hydro")), 0, 40)
-        oled.text("Imports: {}%".format(mix.percent("imports")), 0, 50)
-        oled.show()
-        time.sleep(30)
+        time_now = time.ticks_ms()
+        time_since_last = time.ticks_diff(time_now, last_update) / 1000
+        if time_since_last > 30 or first_run:
+            last_update = time_now
+            mix = energy.mix()
+            ci = energy.intensity()
+            first_run = False
+        update_display(oled, wlan, time_since_last, mix, ci)
+        time.sleep(1)
 
+if __name__ == '__main__':
+    run()
